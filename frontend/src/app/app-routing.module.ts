7a41fb148e4350dab3883e26dde2692ca52dc336
Athanasios Markou <thanasismarko@gmail.com>
2019-03-04 09:09:10 +0000

    Separate homepage html into components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+// Create the routes for the application
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    data: {
+      title: 'Home Page'
+    }
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+    data: {
+      title: 'About Page'
+    }
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+    data: {
+      title: 'Contact Page'
+    }
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+    data: {
+      title: 'The page was not found'
+    }
+  }
+];
 
 @NgModule({
+  // Initialize the router module to use the routes list for the application
   imports: [RouterModule.forRoot(routes)],
+  // Export the router module to be used in the main application
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopContainerComponent } from './home-page/top-container/top-container.component';
+import { WhoAreWeComponent } from './home-page/who-are-we/who-are-we.component';
+import { WhatWeDoComponent } from './home-page/what-we-do/what-we-do.component';
+import { OurWorkComponent } from './home-page/our-work/our-work.component';
+import { OurTeamComponent } from './home-page/our-team/our-team.component';
+import { NewsletterComponent } from './home-page/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NotFoundPageComponent,
+    NavBarComponent,
+    FooterComponent,
+    TopContainerComponent,
+    WhoAreWeComponent,
+    WhatWeDoComponent,
+    OurWorkComponent,
+    OurTeamComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,

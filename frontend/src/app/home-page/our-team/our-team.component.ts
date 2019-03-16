7c28a9df40708d78d05a46dcba70b9e357d88edd
Athanasios Markou <thanasismarko@gmail.com>
2019-03-16 08:49:31 +0000

    feat: Make our team section responsive

--- frontend/src/app/home-page/our-team/our-team.component.ts
+++ frontend/src/app/home-page/our-team/our-team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from 'src/app/team.service';
+import { TeamMember } from 'src/app/team-member';
 
 @Component({
   selector: 'app-our-team',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OurTeamComponent implements OnInit {
 
-  constructor() { }
+  /* Initialize the team members variable */
+  teamMembers: TeamMember[];
+
+  constructor(private teamService: TeamService) { }
 
   ngOnInit() {
+    // Get the members from the teamService
+    this.teamMembers = this.teamService.getMembers();
+
+    // Get the members with http request
+    // this.teamService.getTeamMembers()
+    //   .subscribe(data => console.log(data));
+  }
+
+  handleHover(event: MouseEvent, color: string) {
+    console.log(event);
   }
 
 }
